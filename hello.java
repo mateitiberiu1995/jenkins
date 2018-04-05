463629579f1aade42cb24984ae52836884ee1765
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-04-05 17:31:37 +0100

    we try something

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
+import java.io.*;
+
 public class hello{
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		Scanner scanner = new Scanner(System.in);
+	        double payment = scanner.nextDouble();
+	        scanner.close();
+		System.out.println(payment);
 	}
 }
